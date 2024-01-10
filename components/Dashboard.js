bafd2c6793f961cb9a6c59b9449679b1ffff6213
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-01-10 09:35:45 +0000

    Update 2 files

---
+++ components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Dashboard() {
+
+    return(
+        <div>
+            <h2> Here will be the board Component
+            </h2>
+        </div>
+    )
+}
